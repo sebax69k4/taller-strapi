e51b17e011b3961808e24896992dd4c4468599b8
sebastian rodriguez <sebastian.estudio95@gmail.com>
2025-12-04 01:05:13 -0300

    back s

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,6 +1,33 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+    {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:4321',
+        'http://localhost:4322',
+        'https://taller-strapi-front.onrender.com'  // ← Agrega tu URL de Render aquí
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
