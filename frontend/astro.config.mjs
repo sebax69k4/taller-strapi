236acfa280bd5b357332418d5b6c13230ce3582c
sebastian rodriguez <sebastian.estudio95@gmail.com>
2025-12-03 21:17:04 -0300

    se habilito vercel

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
 
 import tailwindcss from '@tailwindcss/vite';
 
 import node from '@astrojs/node';
 
+import { defineConfig } from 'astro/config';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // O 'hybrid'
   vite: {
     plugins: [tailwindcss()]
   },
